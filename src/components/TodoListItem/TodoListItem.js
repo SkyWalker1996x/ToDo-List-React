e5eff7cf81030c2088b9df1a9d33961563d8c6bf
SkyWalker1996x <chehuta1996@gmail.com>
2020-01-31 22:54:16 +0200

    add the ability to make the task important and accomplished

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -4,24 +4,51 @@ import './TodoListItem.css';
 
 class TodoListItem extends Component{
 
+    state = {
+        done: false,
+        important: this.props.important
+    };
+
+    onLabelChange = () => {
+        this.setState(({done}) => {
+            return {
+                done: !done
+            }
+        })
+    };
+
+    onImportantChange = () => {
+        this.setState(({important}) => {
+            return {
+                important: !important
+            };
+        })
+    };
+
     render() {
-        const {label, important = false} = this.props;
+        const {label} = this.props;
+        const { done, important } = this.state;
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done'
+        }
 
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
             <span
                 className="todo-list-item-label"
                 style={style}
-                onClick={ () => console.log(`Done: ${label}`) }>
+                onClick={ this.onLabelChange }>
                 {label}
             </span>
 
             <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={ this.onImportantChange }>
                     <i className="fa fa-exclamation"/>
             </button>
 
