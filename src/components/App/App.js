c90a94704d2a8d1c640ba1c9a6a135a666380a66
SkyWalker1996x <chehuta1996@gmail.com>
2020-02-01 00:31:37 +0200

    added the ability to remove task

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../AppHeader/AppHeader'
 import SearchPanel from '../SearchPanel/SearchPanel'
 import TodoList from '../TodoList/TodoList'
 import ItemStatusFilter from "../ItemStatusFilter/ItemStatusFilter";
 import './App.css'
 
-const App = () => {
+class App extends Component{
 
-    const todoData = [
-        {id: 1, label: 'Drink Coffee', important: false},
-        {id: 2, label: 'Make Awesome App', important: true },
-        {id: 3, label: 'Have a lunch', important: false}
-    ];
+    state = {
+        todoData: [
+            {id: 1, label: 'Drink Coffee', important: false},
+            {id: 2, label: 'Make Awesome App', important: true },
+            {id: 3, label: 'Have a lunch', important: false}
+        ]
+    };
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    handleDeleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const removeIndex = todoData.findIndex((item) => item.id === id);
+
+            const newState = [
+                ...todoData.slice(0, removeIndex),
+                ...todoData.slice(removeIndex + 1)
+            ];
+
+            return {
+                todoData: newState
+            }
+        })
+    };
+
+    render() {
+        return (
+            <div className="todo-app" onClick={this.test}>
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList todos={this.state.todoData} onDeleted={this.handleDeleteItem}/>
             </div>
-
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
